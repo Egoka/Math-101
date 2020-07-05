d5e29da594fb813ff0c029223f497c82d86fc01d
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-07-06 00:44:53 +0300

    Compression function
    If the picture is larger than 1000 by 1000 pixels, then it is compressed twice.
    Input data:
    * photo - original image
    * height_out - required height
    * width_out - required width
    * bypass - activation variable for compressing small images
    The output is a compressed image.

--- static/Neuron_Network/Network_features/Photo_Processing.py
+++ static/Neuron_Network/Network_features/Photo_Processing.py
@@ -7,3 +7,10 @@ def translate_arr():
     photo = io.imread('static/Neuron_Network/img/paint.png')
     photo = np.sum(photo, axis=2) // photo.shape[2]
     photo = photo.astype('uint8')
+    if photo.shape[0] > 1000 and photo.shape[1] > 1000:
+        photo = compression(photo, int(photo.shape[0] / 2), int(photo.shape[1] / 2), False)
+
+
+################################################################
+def compression(photo, height_out, width_out, bypass):
+    return small_photo

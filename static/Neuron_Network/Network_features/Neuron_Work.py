0e2d7bdad2dca27ee9593f036137e80e98744074
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-07-05 22:28:41 +0300

    Activation function
    Input and output neurons are multiplied by weights, and then passed through the activation function.
    Input Variables:
    * input_weights - input weights
    * output_weights - output weights
    * true_input - input image
    At the output of the function we get the response of the neural network.

--- static/Neuron_Network/Network_features/Neuron_Work.py
+++ static/Neuron_Network/Network_features/Neuron_Work.py
@@ -3,4 +3,14 @@ import scipy.special
 
 
 def operation(input_weights, output_weights, true_input):
+    true_input_t = numpy.array(true_input, ndmin=2).T  # Транспонированная картнка
+    in_matrix = numpy.dot(input_weights, true_input_t)  # Входной сигнал * веса
+    in_finale = activation_function(in_matrix)  # Выходной результат функции входных сигналов
+    out_matrix = numpy.dot(output_weights, in_finale)  # Выходной сигнал * веса
+    out_finale = activation_function(out_matrix)  # Выходной результат функции выходных сигналов
     return out_finale
+
+
+###########################################################
+def activation_function(x):
+    return scipy.special.expit(x)  # Функция активации  f(x) = 1 / (1+e^-x ) Сигмооида

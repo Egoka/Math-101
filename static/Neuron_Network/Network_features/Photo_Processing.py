af7e8adf8aec41183139a1762eaf89b9709da6a7
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-07-06 00:43:39 +0300

    Download image.
    Loads a saved image from the file system.
    Using the sum of the three colors leads the picture in black and white.
    Changes the data type.

--- static/Neuron_Network/Network_features/Photo_Processing.py
+++ static/Neuron_Network/Network_features/Photo_Processing.py
@@ -4,3 +4,6 @@ import math
 
 
 def translate_arr():
+    photo = io.imread('static/Neuron_Network/img/paint.png')
+    photo = np.sum(photo, axis=2) // photo.shape[2]
+    photo = photo.astype('uint8')

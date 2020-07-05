e1955aebb9f5da8880a88ffc2a89784d84861352
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-07-05 21:58:30 +0300

    Test list

--- static/Neuron_Network/Network_features/Neuron_Testing.py
+++ static/Neuron_Network/Network_features/Neuron_Testing.py
@@ -1,3 +1,9 @@
 import numpy
 
+
 def test():
+    file = open('static/Neuron_Network/Mnist_Tables/mnist_test.csv', 'r')
+    training_list = file.readlines()
+    file.close()
+    input_weights = numpy.load('static/Neuron_Network/Numpy_array/Weight_Table_INPUT.npy')
+    output_weights = numpy.load('static/Neuron_Network/Numpy_array/Weight_Table_OUTPUT.npy')

965a0a72ebdc78f727061a0cfae0695356baddb6
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-12 02:58:36 +0300

    An exception
    On the first cycle, arithmetic characters are excluded at the beginning of the list, except for subtraction.
    In the next cycle, all arithmetic signs from the last position are excluded.
    In the beam, if an error occurs on the index, it means that the input list was empty initially. Therefore, you need to display a message about the absence of an example and close the function.

--- static/Neuron_Network/Network_features/Reckoning_Equations.py
+++ static/Neuron_Network/Network_features/Reckoning_Equations.py
@@ -4,4 +4,12 @@ import numpy
 
 def count():
     training_list = numpy.load('static/Neuron_Network/Numpy_array/Symbol.npy')
+    try:
+        while training_list[0] > 9 and training_list[0] != 11:
+            training_list = numpy.delete(training_list, 0)
+        while training_list[-2] > 9:
+            training_list = numpy.delete(training_list, -2)
+    except IndexError:
+        example = "Примера нет."
+        return example
     return example

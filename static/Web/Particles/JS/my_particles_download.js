44e98c74467aa4209ec7ed91ddbf7872ca425af3
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-13 19:14:12 +0300

    Connect an animated background for the download page

---
+++ static/Web/Particles/JS/my_particles_download.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles_download', '../static/Web/Particles/My_settings/my-particles-config_download.json', function() {
+        console.log('callback - particles.js config loaded');
+});
+})

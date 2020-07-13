88a8edd10c01422b914c0b3a43469827bdcdcc6d
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-13 03:07:48 +0300

    Window and pen options
    Sets the window size depending on the screen size.
    Initial line thickness 15 is set.

--- static/Web/js/canvas.js
+++ static/Web/js/canvas.js
@@ -4,4 +4,23 @@ var ongoingTouches = [];
 window.onload = function() {
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
+        if(window.innerWidth>=1000)
+    {
+     width = window.innerWidth*0.8;
+     height = window.innerHeight*0.62;
+    }
+    else{
+    width = window.innerWidth;
+    height = window.innerHeight*0.52;
+    }
+    canvas.width = width;
+    canvas.height = height;
+    context.lineWidth = 15;
+    startup()
+    //////////////////////
+}
+//////////////////////////////////////////////////////////
+function startup(){
+	context.lineCap =['round'];
+	context.lineJoin = ['bevel'];
 }

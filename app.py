1c54c2fb8b8531893ff5d06a483e1a8d2a2eb8a8
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-14 22:58:12 +0300

    Connection of functions and library
    Necessary library links are connected:
    * Flask - a framework for the work of the backend.
    * Pillow - image processing.
    * Scikit-image - a graphic library designed for image processing.
    * base64, BytesIO - needed for image decoding.
    The necessary functions are connected:
    * Calculation - a working neural network is connected.
    * Translate_arr - primary image processing.
    * Fragmentation - secondary image processing.
    * Сount - the function of calculating the equation.

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
+### Flask
 from flask import Flask
+### Download photo
+from skimage import io
+### Painting
+from PIL import Image
+from io import BytesIO
+import base64
+### MY functions
+from static.Neuron_Network.Network_features.Neuron_Work         import calculation
+from static.Neuron_Network.Network_features.Photo_Processing    import translate_arr, fragmentation
+from static.Neuron_Network.Network_features.Reckoning_Equations import count
+
 
 app = Flask(__name__)
 
